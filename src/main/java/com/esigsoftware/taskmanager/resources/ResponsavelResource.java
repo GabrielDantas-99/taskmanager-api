31cbd0577a9a2ad0003a5dad24d9e11b33cdb607
Gabriel Dantas <gabrieldantaslp@gmail.com>
2022-05-14 11:39:56 -0300

    FindAll para Responsaveis usando DTO

--- src/main/java/com/esigsoftware/taskmanager/resources/ResponsavelResource.java
+++ src/main/java/com/esigsoftware/taskmanager/resources/ResponsavelResource.java
@@ -1,5 +1,8 @@
 package com.esigsoftware.taskmanager.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,19 +11,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.esigsoftware.taskmanager.domain.Responsavel;
+import com.esigsoftware.taskmanager.dtos.ResponsavelDTO;
 import com.esigsoftware.taskmanager.service.ResponsavelService;
 
 @RestController
-@RequestMapping(value="/responsaveis")
+@RequestMapping(value = "/responsaveis")
 public class ResponsavelResource {
-	
+
 	@Autowired
 	private ResponsavelService service;
 
+	// Retornando responsaveis por id
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Responsavel> findById(@PathVariable Integer id) {
 		Responsavel obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
-	
+
+	// Retornando apenas apenas os responsaveis
+	@GetMapping
+	public ResponseEntity<List<ResponsavelDTO>> findAll() {
+		List<Responsavel> list = service.findAll();
+		List<ResponsavelDTO> listDTO = list.stream().map(obj -> new ResponsavelDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+	}
 }
--- src/main/java/com/esigsoftware/taskmanager/service/ResponsavelService.java
+++ src/main/java/com/esigsoftware/taskmanager/service/ResponsavelService.java
@@ -1,5 +1,6 @@
 package com.esigsoftware.taskmanager.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,4 +23,7 @@ public class ResponsavelService {
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Responsavel.class.getName()));
 	}
 
+	public List<Responsavel> findAll() {
+		return repository.findAll();
+	}
 }
